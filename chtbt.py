c8da434816f58c45bdc06d3f389d93af2ca95d3a
Rahul <suryarising17@gmail.com>
2022-06-06 15:32:07 +0530

    adedd logic

--- chtbt.py
+++ chtbt.py
@@ -10,60 +10,23 @@ def hello():
     return "Hello, World!"
 
 @app.route("/cht", methods=['POST'])
-def sms_reply():
+def bot():
     """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("im rahul: {}".format(msg))
+    # resp.message("im rahul: {}".format(msg))
+    msg = bot_resp.message()
+    if 'hello' in user_msg:
+        msg.body("Hi there! How may I help you?")
+    else:
+        msg.body("Valid Question for this ")
 
-    return str(resp)
+
+    return str(bot_resp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-# from flask import Flask,request
-# from googlesearch import search
-# import requests
-# from twilio.twiml.messaging_response import MessagingResponse
-  
-  
-# app = Flask(__name__)
-  
-# @app.route("/", methods=["POST"])
-  
-# # chatbot logic
-# def bot():
-  
-#     # user input
-#     user_msg = request.values.get('Body', '').lower()
-  
-#     # creating object of MessagingResponse
-#     response = MessagingResponse()
-  
-#     # User Query
-#     q = user_msg + "geeksforgeeks.org"
-  
-#     # list to store urls
-#     result = []
-  
-#     # searching and storing urls
-#     for i in search(q, tld='co.in', num=6, stop=6, pause=2):
-#         result.append(i)
-  
-#     # displaying result
-#     msg = response.message(f"--- Result for '{user_msg}' are  ---")
-  
-#     msg = response.message(result[0])
-#     msg = response.message(result[1])
-#     msg = response.message(result[2])
-#     msg = response.message(result[3])
-#     msg = response.message(result[4])
-  
-#     return str(response)
-  
-  
-# if __name__ == "__main__":
-#     app.run()
+    # app.run(debug=True)
+    app.run()
