044cb4b8f8c5a810c99b2dbdf804a72b09175ed4
Rahul <suryarising17@gmail.com>
2022-06-06 15:27:13 +0530

    solove bug1

--- chtbt.py
+++ chtbt.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 from googlesearch import search
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
@@ -23,3 +23,47 @@ def sms_reply():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# from flask import Flask,request
+# from googlesearch import search
+# import requests
+# from twilio.twiml.messaging_response import MessagingResponse
+  
+  
+# app = Flask(__name__)
+  
+# @app.route("/", methods=["POST"])
+  
+# # chatbot logic
+# def bot():
+  
+#     # user input
+#     user_msg = request.values.get('Body', '').lower()
+  
+#     # creating object of MessagingResponse
+#     response = MessagingResponse()
+  
+#     # User Query
+#     q = user_msg + "geeksforgeeks.org"
+  
+#     # list to store urls
+#     result = []
+  
+#     # searching and storing urls
+#     for i in search(q, tld='co.in', num=6, stop=6, pause=2):
+#         result.append(i)
+  
+#     # displaying result
+#     msg = response.message(f"--- Result for '{user_msg}' are  ---")
+  
+#     msg = response.message(result[0])
+#     msg = response.message(result[1])
+#     msg = response.message(result[2])
+#     msg = response.message(result[3])
+#     msg = response.message(result[4])
+  
+#     return str(response)
+  
+  
+# if __name__ == "__main__":
+#     app.run()
