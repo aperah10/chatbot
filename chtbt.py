c6aee8477cb6b84b62c5287371eba119a2fcea4e
Rahul <suryarising17@gmail.com>
2022-06-06 17:29:13 +0530

    change method

--- chtbt.py
+++ chtbt.py
@@ -18,12 +18,11 @@ def bot():
     msg = bot_resp.message()
     user_msg = request.form.get('Body').lower()
     
-    while True:
-        msg.body("Are you a Corn Grower ?")
+    msg.body("Are you a Corn Grower ?")
         
           
          
-        if 'yes' or"Yes" in user_msg:
+    if 'yes' or"Yes" in user_msg:
             msg.body("Did You Know that Fortenza Duo can secure your plant stand ?")
             msg.body("videos1")
             msg.body("videos2")
@@ -32,7 +31,7 @@ def bot():
             if 'videos1' in user_msg:
                 msg.body("videos1")
     
-        elif 'no' or"No" in user_msg:
+    elif 'no' or"No" in user_msg:
             msg.body("Do you a Grow any other crop ?")
             msg.body("Yes")
             msg.body("No") 
