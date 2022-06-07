c0a875e4de8c254e979f81150b4896baf5ca096d
Rahul <suryarising17@gmail.com>
2022-06-07 18:01:36 +0530

    added new logic

---
+++ check1.py
@@ -0,0 +1,12 @@
+print("hello seven")
+
+def bots():
+    while True:
+        a= input("Are you Corn Grower ?")
+        if 'yes' in a:
+           b= input("Did you know that Fortenza Duo can secure your plant stand?")
+           print(b)
+        else:
+            n1= input("Do you grow any other crop?")
+
+bots()
--- chtbt.py
+++ chtbt.py
@@ -2,40 +2,53 @@ from flask import Flask,request
 from googlesearch import search
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
-  
-app = Flask(__name__)
+from fastapi import FastAPI 
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+app =FastAPI()
 
-@app.route("/cht", methods=['POST'])
-def bot():
+@app.get("/")
+async def hello():
+    return "welcome page"
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
+
+# @app.route("/cht", methods=['POST'])
+@app.post("/cht")
+async def bot(user_msg):
     """Respond to incoming calls with a simple text message."""
     
     # Fetch the message
-    user_msg = request.form.get('Body').lower()
+    
     bot_resp= MessagingResponse()
     msg = bot_resp.message()
     
     
-    msg.body("Are you a Corn Grower ?")
+    msg.body("Are you a Corn Grower ? \n yes \n no")
+
+    # user_msg = request.form.get('Body').lower()
         
-          
-         
     if 'yes' in user_msg:
             msg.body("Did You Know that Fortenza Duo can secure your plant stand ?")
-            msg.body("videos1")
-            msg.body("videos2")
-            msg.body("videos3")
+            msg.body("\n 1. if  you want to manage early season pests")
+            msg.body("\n 2. if  you want to have excellent establishment")
+            msg.body("\n 3. if  you want better yields")
 
-            if 'videos1' in user_msg:
+            if '1' in user_msg:
                 msg.body("videos1")
+            if '2' in user_msg:
+                msg.body("videos2")
+            if '3' in user_msg:
+                msg.body("videos3") 
     
-    elif 'no' or"No" in user_msg:
+    elif 'no' in user_msg:
             msg.body("Do you a Grow any other crop ?")
             msg.body("Yes")
             msg.body("No") 
+
+    msg.body("Do you want to purchase FD ? \n yes \n no") 
 
             
 
@@ -43,6 +56,7 @@ def bot():
 
 
     return str(bot_resp)
+    # return user_msg
 
 if __name__ == "__main__":
     # app.run(debug=True)
