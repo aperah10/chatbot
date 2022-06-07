247df6c8e48f636fae10171ed9263b88ddf697db
Rahul <suryarising17@gmail.com>
2022-06-07 18:54:51 +0530

    added fastapi

--- chtbt.py
+++ chtbt.py
@@ -6,18 +6,18 @@ from fastapi import FastAPI
 
 app =FastAPI()
 
-# @app.get("/")
-# async def hello():
-#     return "welcome page"
-app = Flask(__name__)
-
-@app.route("/")
+@app.get("/")
 def hello():
-    return "Hello, World!"
+    return "welcome page"
+# app = Flask(__name__)
+
+# @app.route("/")
+# def hello():
+#     return "Hello, World!"
 
 @app.route("/cht", methods=['POST'])
 # @app.post("/cht")
-async def bot():
+def bot():
     """Respond to incoming calls with a simple text message."""
     
     # Fetch the message
@@ -34,25 +34,34 @@ async def bot():
         msg.body("Are you a Corn Grower ? \n yes  \n no ")
         
     if "yes" in user_msg:
-            msg.body("Did You Know that Fortenza Duo can secure your plant stand ?")
-            msg.body("\n 1. if  you want to manage early season pests")
-            msg.body("\n 2. if  you want to have excellent establishment")
-            msg.body("\n 3. if  you want better yields")
+            msg.body("""Did You Know that Fortenza Duo can secure your plant stand ? 
+            \n 1. if  you want to manage early season pests
+            \n 2. if  you want to have excellent establishment
+            \n 3. if  you want better yields""")
+            # msg.body("")
+            # msg.body("")
+            # msg.body("")
 
-            if '1' in user_msg:
+    if '1' in user_msg:
                 msg.body("videos1")
-            if '2' in user_msg:
+                msg.body("Do you want to purchase FD ? \n yes \n no") 
+
+    if '2' in user_msg:
                 msg.body("videos2")
-            if '3' in user_msg:
+                msg.body("Do you want to purchase FD ? \n yes \n no") 
+
+    if '3' in user_msg:
                 msg.body("videos3") 
+                msg.body("Do you want to purchase FD ? \n yes \n no") 
+        
     
     elif 'no' in user_msg:
-            msg.body("Do you a Grow any other crop ?")
-            msg.body("Yes")
-            msg.body("No") 
+            msg.body("Do you a Grow any other crop ? ")
+            # msg.body("Yes")
+            # msg.body("No") 
 
-    # msg.body("Do you want to purchase FD ? \n yes \n no") 
-
+     
+    
             
 
    
